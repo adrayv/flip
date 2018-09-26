57a3b10dfa7a69ab6cc75ad2d92f1c664a19d0f8
Destin Raymundo <draym001@g.ucla.edu>
2018-09-25 20:20:56 -0700

    integrated immer with redux

--- src/store.js
+++ src/store.js
@@ -1,19 +1,17 @@
 import { createStore } from 'redux' 
+import produce from 'immer'
 
 const initialState = {
 	test: 1
 }
 
 const reducer = (state = initialState, action) => {
-	switch(action.type){
-		case "TEST":
-			return {
-				...state,
-				test: state.test + 1
-			}
-		default:
-			return state
-	}
+	return produce(state, draft => {
+		switch(action.type){
+			case "TEST":
+				draft.test += 10
+		}
+	})
 }
 
 export default createStore(reducer)
